d78f5e46713ac86fe684a66299222839a48c7399
dnvd <>
2022-05-14 18:53:14 -0500

    added multiple new scripts for quests adjusted quest doors

--- data/movements/scripts/quest_door.lua
+++ data/movements/scripts/quest_door.lua
@@ -3,7 +3,7 @@ function onStepIn(creature, item, position, fromPosition)
 		return false
 	end
 
-	if creature:getStorageValue(item.actionid) == -1 then
+	if creature:getStorageValue(item.actionid) == -1 and item.actionid ~= 0 then
 		creature:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The door seems to be sealed against unwanted intruders.")
 		creature:teleportTo(fromPosition, true)
 		return false
