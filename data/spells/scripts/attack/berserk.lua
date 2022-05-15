aedfb5028b06d1f193dab4ea7795cfe43380c5dd
dnvd <>
2022-05-14 21:19:07 -0500

    adjusted spell formulas

--- data/spells/scripts/attack/berserk.lua
+++ data/spells/scripts/attack/berserk.lua
@@ -5,24 +5,16 @@ combat:setParameter(COMBAT_PARAM_BLOCKARMOR, true)
 combat:setParameter(COMBAT_PARAM_USECHARGES, true)
 combat:setArea(createCombatArea(AREA_SQUARE1X1))
 
-function onGetFormulaValues(player, skill, attack, factor)
-	local min = (player:getLevel() / 5) + (skill * attack * 0.03) + 7
-	local max = (player:getLevel() / 5) + (skill * attack * 0.05) + 11
-	return -min, -max
+function onGetFormulaValues(player, magicLevel)
+	min = -(level * 2.2)
+	max = -(level * 3.85)
+--	min = -((level * 2) + (magicLevel * 3)) * 1.4
+--	max = -((level * 2) + (magicLevel * 3)) * 1.65
+	return min, max
 end
 
 combat:setCallback(CALLBACK_PARAM_SKILLVALUE, "onGetFormulaValues")
 
 function onCastSpell(creature, variant)
-	local manaCost = creature:getLevel() * 4
-	if creature:getMana() < manaCost and not creature:hasFlag(PlayerFlag_HasInfiniteMana) then
-		creature:sendCancelMessage(RETURNVALUE_NOTENOUGHMANA)
-		creature:getPosition():sendMagicEffect(CONST_ME_POFF)
-		return false
-	end
-
-	creature:addManaSpent(manaCost)
-	creature:addMana(-manaCost)
-
 	return combat:execute(creature, variant)
 end
